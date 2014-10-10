8051579d8dc665e538d36927d880aa618f924234
Samuel <s-bridge@live.co.uk>
2014-10-10 20:40:15 +0100

    [JS] - Added in base plugin template, revised

--- js/src/zRS.js
+++ js/src/zRS.js
@@ -2,4 +2,86 @@
 	
 	'use strict';
 
+	var version = '0.1',
+		name = 'zRS';
+
+	$.fn.zRS = function(settings, params) {
+		
+		var results = [];
+
+		for(var i = 0; i < this.length; i++) {
+
+			var self = $(this[i]);
+
+			if(!self.data('ins')) {
+
+				if(typeof settings == 'string' && console) {
+
+					console.error('['+ name +' '+ version +'] - not running, try firing methods after initialisation'); 
+					continue;
+
+				}
+
+				var ins = new zRS_core(self, settings);
+
+				self.data('ins', ins);
+
+			} else {
+
+				var ins = self.data('ins');
+
+				if(ins['public'][settings]) {
+
+					if(this.length > 1) {
+
+						results.push(ins['public'][settings](params));
+
+					} else {
+
+						return ins['public'][settings](params);
+						
+					}
+
+				} else {
+
+					if(console) console.warn('['+ name +' '+ version +'] - '+ settings +' is not a public method');
+
+				}
+
+			}
+
+		}
+
+		if(results.length >= 1) return results;
+
+	}
+
+	var zRS_core = function(self, settings) {
+
+		var ins = this;
+
+		var secret = {
+
+			init: function() {
+
+
+			}
+
+		}
+
+		ins.public = {
+
+
+		}
+
+		secret.init();
+
+	}
+
 })();
+
+$(document).ready(function(){
+
+	$('.slider').zRS();
+
+});
